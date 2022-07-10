0b0650afc1c61f456c88cd8765944fd3600dd351
F_B <77896476+LP123321@users.noreply.github.com>
2022-07-10 09:43:54 -0400

    Update SolutionExtracter.py
    Clarify documentation

--- Methods/SolutionExtracter.py
+++ Methods/SolutionExtracter.py
@@ -22,73 +22,59 @@ str str str str ...
 # variables are in an equality chain
 # I/O: 2d list / dict (terms : coefficients)
 def lineUp(matrix):
-
-    res = {}
-
+    res = {} # Dict ('term' : coefficient)
     og_matrix = matrix.copy()
-
-    # Deal with zero coefficients
+    # Add x to result where x = ('term' : 0)
     for n in range(len(og_matrix)-1):
         if og_matrix[n][-1] == 0:
             res[og_matrix[-1][n]] = 0
-
             matrix.remove(og_matrix[n])
-
-
-
     # Find the coefficient of the last term to unify all equivalences
+    '''
+    Ex:
+    An input matrix encodes equations of form:
+    
+    Na = Kz
+    Mb = Ez
+    Pc = Cz
+    
+    For integer {N M P K E C} wher {a b c z} are the coefficients. This step finds {A B C D Z} such that
+    
+    Aa = Bb = Cc = Zz
+    '''
     facs = []
-    for n in range(len(matrix)-1):
-        facs.append(matrix[n][-1])
-
+    for n in range(len(matrix)-1):  # Loop over int part of matrix
+        facs.append(matrix[n][-1])  # Extract last column's value
+    # Find LCM of extracted values
     lcm = facs[0]
     for num in facs[1:]:
         lcm = math.lcm(num, lcm)
-
-
-    # Set up the list
+    # Add terms' adjusted coefficients to result 
+    # (symbolic of an equality chain e.g. Aa = Bb = ... )
     for i in range(len(matrix)-1):
         res[matrix[-1][i]] = int(-lcm/matrix[i][-1] * matrix[i][i])
-
-
     res[matrix[-1][-1]] = lcm
-
-
     return res
 
-
-# TODO
 # Given the equality chain, returns a dict with the terms and their coefficients
 # I/O: 1d list / 1d list
 def primeSolution(lineUp):
-
     terms = list(lineUp)
     og_terms = terms.copy()
-    
     primeSol = {}
-
-
-
-    # Bypass zero coefficients
+    # Remove zero coefficients from lineup
     for k in range(len(og_terms)):
         if lineUp[og_terms[k]] == 0:
             primeSol[og_terms[k]] = 0
-
             terms.remove(og_terms[k])
-
-
     # Find LCM of coefficients
     lcm = lineUp[terms[0]]
     for key in terms[1:]:
         lcm = math.lcm(lineUp[key], lcm)
-
-
-    # Each term's coefficient must be the quotient of the LCM and their coefficients
+    # Each term's coefficient must be the quotient of the LCM and its coefficient in the chain
     for i in range(len(terms)):
         term = terms[i]
         primeSol[term] = int(lcm / lineUp[term])
-
-
     return primeSol
 
 
